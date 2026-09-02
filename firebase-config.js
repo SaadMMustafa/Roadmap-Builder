40c32b971fd290d8ee0efed813f7a2fca86743a1
Saad M. Mustafa <60714248+SaadMMustafa@users.noreply.github.com>
2026-09-02 13:04:12 +0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCC2hPWHn1N0mAJHkk0e05b_zsE5oiHCnI',
-  authDomain: 'roadmap-builder-1a792.firebaseapp.com',
-  projectId: 'roadmap-builder-1a792',
-  storageBucket: 'roadmap-builder-1a792.firebasestorage.app',
-  messagingSenderId: '1071125400049',
-  appId: '1:1071125400049:web:0503f8e50569f90d8e1c50'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCC2hPWHn1N0mAJHkk0e05b_zsE5oiHCnI",
+  authDomain: "roadmap-builder-1a792.firebaseapp.com",
+  projectId: "roadmap-builder-1a792",
+  storageBucket: "roadmap-builder-1a792.firebasestorage.app",
+  messagingSenderId: "1071125400049",
+  appId: "1:1071125400049:web:0503f8e50569f90d8e1c50",
+  measurementId: "G-T1PR4L8RMZ"
 };
-
-export const ownerEmail = 'saadmmustafa99@gmail.com';
